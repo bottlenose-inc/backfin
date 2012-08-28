499158644a70c8e8038553b8e903a862617c1124
Kaare Larsen <kaare.a.larsen@gmail.com>
2012-08-28 17:03:11 +0200

    fixed code ...

--- src/core.js
+++ src/core.js
@@ -9,7 +9,7 @@
 // * [Nicholas Zakas: Scalable JavaScript Application Architecture](http://www.youtube.com/watch?v=vXjVFPosQHw&feature=youtube_gdata_player)
 // * [Writing Modular JavaScript: New Premium Tutorial](http://net.tutsplus.com/tutorials/javascript-ajax/writing-modular-javascript-new-premium-tutorial/)
 // include 'deferred' if using zepto
-define('backfin-core',['jquery', 'underscore'],function($, _) {
+define('backfin-core',function($, _) {
   "use strict";
 
   var core = {}; // Mediator object
