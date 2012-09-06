3097b77ee9a829ced3180ac8aadb7ac9c14289f3
Kaare Larsen <kaare.a.larsen@gmail.com>
2012-09-06 17:16:52 +0200

    fixed up hotswap for less

--- src/hotswap.js
+++ src/hotswap.js
@@ -23,10 +23,34 @@ define('backfin-hotswap', ['backfin-core'], function(backfin){
     }.bind(this), this._connect.bind(this)); 
   }
 
+  Hotswap.prototype._getRootPath = function(key) {
+    return key.replace('plugins/', '').replace(/\/[^/]*$/, '');
+  }
+
   Hotswap.prototype._handleResponse = function(res) {
     var self = this;
+
     //xxx not perfect should allow for css to reload as well
-    if(res.less && Object.keys(res.less) &&  window.less) less.refresh();
+    if(res.less && Object.keys(res.less) && window.less) Object.keys(res.less).forEach(function(key){
+      var headNode = requirejs.s.head;
+      var link = document.getElementById(key);
+      if(link) {
+        link.href = key + '?bust=' + Date.now();
+        less.refresh();
+        return;
+      } 
+
+      var link = document.createElement('link');
+      link.id = key;
+      link.setAttribute('rel', 'stylesheet/less');
+      link.setAttribute('type', 'text/css');
+      link.href = key;
+      headNode.appendChild(link);
+      less.sheets.push(link);
+      less.refresh();
+    })
+
+    
 
     if(res.plugins) {
       var manifests = backfin.getManifests();
@@ -37,7 +61,7 @@ define('backfin-hotswap', ['backfin-core'], function(backfin){
 
       try {
         Object.keys(res.plugins).forEach(function(key) {
-          var id = key.replace('plugins/', '').replace(/\/[^/]*$/, '');
+          var id = self._getRootPath(key);
           var plugin = plugins[id];
           if(plugin) {
             self._reloadPlugin(id);
