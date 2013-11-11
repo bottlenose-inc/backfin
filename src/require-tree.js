cf7599c40c65ff548ae9dbc685bfdc966c721a64
Kaare Larsen <kaare.a.larsen@gmail.com>
2013-11-11 14:17:16 +0100

    fixed up module

--- src/require-tree.js
+++ src/require-tree.js
@@ -1,26 +1,24 @@
-define(function(){
-  requirejs.onResourceLoad = function (context, map, depMaps) {
-    if (!window.rtree) {
-      window.rtree = {
-        tree: {}
-      };
-    }
+requirejs.onResourceLoad = function (context, map, depMaps) {
+  if (!window.rtree) {
+    window.rtree = {
+      tree: {}
+    };
+  }
 
-    var tree = window.rtree.tree;
+  var tree = window.rtree.tree;
 
-    function Node() {
-      this.deps = [];
-    }
+  function Node() {
+    this.deps = [];
+  }
 
-    if (!tree[map.name]) {
-      tree[map.name] = new Node();
-    }
+  if (!tree[map.name]) {
+    tree[map.name] = new Node();
+  }
 
-    // For a full dependency tree
-    if (depMaps) {
-      for (var i = 0; i < depMaps.length; ++i) {
-        tree[map.name].deps.push(depMaps[i].name);
-      }
+  // For a full dependency tree
+  if (depMaps) {
+    for (var i = 0; i < depMaps.length; ++i) {
+      tree[map.name].deps.push(depMaps[i].name);
     }
-  };
-});
+  }
+};
